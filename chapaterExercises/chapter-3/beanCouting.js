00913b6daf353d12fb78e65d9e948d828f5aa910
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-08-25 17:30:54 +0530

    Update beanCouting.js

--- chapaterExercises/chapter-3/beanCouting.js
+++ chapaterExercises/chapter-3/beanCouting.js
@@ -14,3 +14,17 @@ function countBs(string) {
   // Return the count of uppercase B characters.
   return count;
 }
+
+// Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase B characters). Rewrite countBs to make use of this new function.
+
+function countChar(string, char){
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
