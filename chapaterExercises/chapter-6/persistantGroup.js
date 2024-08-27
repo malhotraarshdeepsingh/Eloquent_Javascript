660efca82eb22c5e1c369e240eb19b3c80297069
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-08-27 18:58:39 +0530

    chapter-8 solved

--- chapaterExercises/chapter-6/persistantGroup.js
+++ chapaterExercises/chapter-6/persistantGroup.js
@@ -1,6 +1,6 @@
 // EXERCISE---------------------------------------------------------------7.3
 
-// Most data structures provided in a standard JavaScript environment aren’t very well suited for persistent use. Arrays have slice and concat methods, which allow us to easily create new arrays without damaging the old one. But Set, for example, has no methods for creating a new set with an item added or removed. 
+// Most data structures provided in a standard JavaScript environment aren’t very well suited for persistent use. Arrays have slice and concat methods, which allow us to easily create new arrays without damaging the old one. But Set, for example, has no methods for creating a new set with an item added or removed.
 
 // Write a new class PGroup, similar to the Group class from Chapter 6, which stores a set of values. Like Group, it has add, delete, and has methods. Its add method, however, should return a new PGroup instance with the given member added and leave the old one unchanged. Similarly, delete should create a new instance without a given member.
 
@@ -10,3 +10,36 @@
 
 // Why do you need only one PGroup.empty value rather than having a function that creates a new, empty map every time?
 
+class PGroup {
+  #members;
+  constructor(members) {
+    this.#members = members;
+  }
+
+  add(value) {
+    if (this.has(value)) return this;
+    return new PGroup(this.#members.concat([value]));
+  }
+
+  delete(value) {
+    if (!this.has(value)) return this;
+    return new PGroup(this.#members.filter((m) => m !== value));
+  }
+
+  has(value) {
+    return this.#members.includes(value);
+  }
+
+  static empty = new PGroup([]);
+}
+
+let a = PGroup.empty.add("a");
+let ab = a.add("b");
+let b = ab.delete("a");
+
+console.log(b.has("b"));
+// → true
+console.log(a.has("b"));
+// → false
+console.log(b.has("a"));
+// → false
